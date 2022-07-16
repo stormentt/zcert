88beb0d95176d171cf594ee3373f0cb89c5398c5
Tanner Storment <tanner@secrecy.email>
2022-07-16 13:47:56 -0500

    use logrus for gin logging

--- server/server.go
+++ server/server.go
@@ -4,8 +4,10 @@ import (
 	"crypto/ed25519"
 	"crypto/x509"
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/stormentt/zcert/middleware"
 	"github.com/stormentt/zcert/util"
@@ -34,15 +36,39 @@ func setup() error {
 	return nil
 }
 
+func ginLogger(c *gin.Context) {
+	start := time.Now()
+	path := c.Request.URL.Path
+	c.Next()
+
+	elapsed := time.Since(start)
+
+	log.WithFields(log.Fields{
+		"method":  c.Request.Method,
+		"path":    path,
+		"elapsed": elapsed,
+		"status":  c.Writer.Status(),
+	}).Info("")
+}
 func Serve() error {
 	err := setup()
 	if err != nil {
 		return err
 	}
 
-	r := gin.Default()
-	r.GET("/ca", getCA)
+	r := gin.New()
+	r.Use(ginLogger)
+	r.Use(gin.Recovery())
+	r.SetTrustedProxies(nil)
 
+	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
+		log.WithFields(log.Fields{
+			"method":  httpMethod,
+			"handler": handlerName,
+		}).Debug(absolutePath)
+	}
+
+	r.GET("/ca", getCA)
 	authRoutes := r.Group("/", middleware.CheckAuth)
 	authRoutes.POST("/sign", signCert)
 
